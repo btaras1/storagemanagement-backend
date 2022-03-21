9f6dc376a98a0fdb1abf951f8d088b4438b2d3e3
btaras1 <taras.bruno@gmail.com>
2022-03-21 01:30:05 +0100

    First commit

---
+++ src/main/java/com/management/storage/StorageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.management.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/storage/StorageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
